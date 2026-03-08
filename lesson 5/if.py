9aaece802b3676e0ac1e8983d9b8ac23f616c6fc
L6-69XS <mulingeimmaculate@gmail.com>
2026-03-08 16:06:57 +0300

    Add files via upload

---
+++ lesson 5/if.py
@@ -0,0 +1,8 @@
+num = 3
+if num > 0:
+    print(num, "is a psitive number")
+
+
+num = -3
+if num > 0:
+    print(num, "is a psitive number")
---
+++ lesson 5/prifit-loss.py
@@ -0,0 +1,15 @@
+actual_cost =float(input("please enter the actual price"))
+sale_amount = float(input("please enter the sales amount"))
+
+if (sale_amount > actual_cost):
+    amount=sale_amount-actual_cost
+    print("total profit ={0}".format(amount))
+else:
+    print("no profit")
+i = int(input("enter a number"))
+int(input("enter a number"))
+if(i < 15):
+    print ("i is a smaller number than 15")
+    print ("i'm in if block")
+else:
+    print ("i is grater that 15     ")
